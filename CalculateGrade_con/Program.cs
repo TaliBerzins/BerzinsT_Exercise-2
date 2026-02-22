dc7d2d5ce942ca99bf4d56667e6315cb09b5bb91
tberzins <tberzins@CITDB-06>
2026-02-22 16:51:06 -0500

    Add project files.

---
+++ CalculateGrade_con/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ReadLine("Enter Grade Point: ");
+double numberGrade = Convert.ToDouble(Console.ReadLine());
+String letterGrade;
+if (numberGrade >= 88)
+
+{
+
+    letterGrade = "A";
+
+}
+
+else if (numberGrade >= 80 && numberGrade <= 87)
+
+{
+
+    letterGrade = "B";
+
+}
+
+else if (numberGrade >= 68 && numberGrade <= 79)
+
+{
+
+    letterGrade = "C";
+
+}
+
+else if (numberGrade >= 60 && numberGrade <= 67)
+
+{
+
+    letterGrade = "D";
+
+}
+
+else
+
+{
+
+    letterGrade = "F";
+
+}
+Console.WriteLine($"The letter grade is: {letterGrade}");
+
---
+++ WinFormsApp_Exercise2/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WinFormsApp_Exercise2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtNumberGrade = new TextBox();
+            txtLetterGrade = new TextBox();
+            btnCalculate = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(43, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Number grade:";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(43, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Letter grade:";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // txtNumberGrade
+            // 
+            txtNumberGrade.Location = new Point(152, 44);
+            txtNumberGrade.Name = "txtNumberGrade";
+            txtNumberGrade.Size = new Size(100, 23);
+            txtNumberGrade.TabIndex = 1;
+            // 
+            // txtLetterGrade
+            // 
+            txtLetterGrade.Location = new Point(152, 79);
+            txtLetterGrade.Name = "txtLetterGrade";
+            txtLetterGrade.ReadOnly = true;
+            txtLetterGrade.Size = new Size(100, 23);
+            txtLetterGrade.TabIndex = 2;
+            txtLetterGrade.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(43, 142);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(75, 23);
+            btnCalculate.TabIndex = 2;
+            btnCalculate.Text = "Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(152, 142);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // Form1
+            // 
+            AcceptButton = btnCalculate;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnExit;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnExit);
+            Controls.Add(btnCalculate);
+            Controls.Add(txtLetterGrade);
+            Controls.Add(txtNumberGrade);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculate Letter Grade";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtNumberGrade;
+        private TextBox txtLetterGrade;
+        private Button btnCalculate;
+        private Button btnExit;
+    }
+}
